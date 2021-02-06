82fa565192de626f88ec1b981bfb4159a835401b
jmargielewski <kuba.margielewski@gmail.com>
2021-02-06 10:29:46 +0000

    added Preview and CodeCell components

---
+++ src/components/CodeCell.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+import CodeEditor from './CodeEditor';
+import Preview from './Preview';
+import Resizable from './Resizable';
+
+import bundle from '../bundler';
+
+const CodeCell = () => {
+  const [code, setCode] = useState('');
+  const [input, setInput] = useState('');
+
+  const onClick = async () => {
+    const output = await bundle(input);
+
+    setCode(output);
+  };
+
+  return (
+    <Resizable direction="vertical">
+      <div>
+        <CodeEditor initialValue="" onChange={(value) => setInput(value)} />
+        <div>
+          <button onClick={onClick}>Submit</button>
+        </div>
+        <Preview code={code} />
+      </div>
+    </Resizable>
+  );
+};
+
+export default CodeCell;
