e00fb188a3968b311e39d38ab0265eccb06a3636
jmargielewski <kuba.margielewski@gmail.com>
2021-02-13 21:22:14 +0000

    fetch cells from api

--- src/components/CellList.tsx
+++ src/components/CellList.tsx
@@ -1,12 +1,20 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useTypedSelector } from '../hooks/useTypesSelector';
 import CellListItem from './CellListItem';
 import AddCell from './AddCell';
+import { useActions } from '../hooks/useActions';
+import './cellList.css';
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
     order.map((id) => data[id]),
   );
+
+  const { fetchCells } = useActions();
+
+  useEffect(() => {
+    fetchCells();
+  }, []);
 
   const renderedCells = cells.map((cell) => (
     <Fragment key={cell.id}>
@@ -16,7 +24,7 @@ const CellList: React.FC = () => {
   ));
 
   return (
-    <div>
+    <div className="cell-list">
       <AddCell forceVisible={cells.length === 0} prevCellId={null} />
       {renderedCells}
     </div>
